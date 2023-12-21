d448e2cca12c1b737194c6f44423efb976907b1e
malanant <133130221+malanant@users.noreply.github.com>
2023-12-21 22:34:29 +0100

    Update main.py

--- calculatrice/main.py
+++ calculatrice/main.py
@@ -4,35 +4,89 @@ customtkinter.set_appearance_mode("System")  # Modes: system (default), light, d
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
 tk = customtkinter.CTk()  # create CTk window like you do with the Tk window
-tk.geometry("350x600")
+tk.geometry("350x490")
 
 def button_function():
     print("button pressed")
 
+label = customtkinter.CTkLabel(tk, text="CTkLabel", fg_color="transparent")
+
+#creation de la zone de texte 
+texte = customtkinter.CTkEntry(tk, text_color='#F875AA', fg_color='#123', border_color='#fff', width=335, height=60)
+texte.place(x=10, y=10)
+
+
 # Use CTkButton instead of tkinter Button
 pourcent = customtkinter.CTkButton(master=tk, text="%", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-pourcent.place(x=10, y=10)
+pourcent.place(x=10, y=85)
 
 c = customtkinter.CTkButton(master=tk, text="C", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-c.place(x=95, y=10)
+c.place(x=95, y=85)
 
 ce = customtkinter.CTkButton(master=tk, text="CE", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-ce.place(x=180, y=10)
+ce.place(x=180, y=85)
 
 DEL = customtkinter.CTkButton(master=tk, text="DEL", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-DEL.place(x=265, y=10)
+DEL.place(x=265, y=85)
 
 sur1 = customtkinter.CTkButton(master=tk, text="1/x", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-sur1.place(x=10, y=75)
+sur1.place(x=10, y=150)
 
 carre = customtkinter.CTkButton(master=tk, text="x²", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-carre.place(x=95, y=75)
+carre.place(x=95, y=150)
 
 bouton7 = customtkinter.CTkButton(master=tk, text="²√x", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
-bouton7.place(x=180, y=75)
+bouton7.place(x=180, y=150)
 
+sur = customtkinter.CTkButton(master=tk, text="/", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+sur.place(x=265, y=150)
 
+sept = customtkinter.CTkButton(master=tk, text="7", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+sept.place(x=10, y=215)
 
+huit = customtkinter.CTkButton(master=tk, text="8", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+huit.place(x=95, y=215)
 
+neuf = customtkinter.CTkButton(master=tk, text="9", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+neuf.place(x=180, y=215)
+
+x = customtkinter.CTkButton(master=tk, text="X", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+x.place(x=265, y=215)
+
+quatre = customtkinter.CTkButton(master=tk, text="4", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+quatre.place(x=10, y=280)
+
+cinq = customtkinter.CTkButton(master=tk, text="5", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+cinq.place(x=95, y=280)
+
+six = customtkinter.CTkButton(master=tk, text="6", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+six.place(x=180, y=280)
+
+moins = customtkinter.CTkButton(master=tk, text="-", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+moins.place(x=265, y=280)
+
+un = customtkinter.CTkButton(master=tk, text="1", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+un.place(x=10, y=345)
+
+deux = customtkinter.CTkButton(master=tk, text="2", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+deux.place(x=95, y=345)
+
+trois = customtkinter.CTkButton(master=tk, text="3", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+trois.place(x=180, y=345)
+
+plus = customtkinter.CTkButton(master=tk, text="+", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+plus.place(x=265, y=345)
+
+signe = customtkinter.CTkButton(master=tk, text="+/-", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+signe.place(x=10, y=410)
+
+zero = customtkinter.CTkButton(master=tk, text="0", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+zero.place(x=95, y=410)
+
+virgule = customtkinter.CTkButton(master=tk, text=",", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+virgule.place(x=180, y=410)
+
+egal = customtkinter.CTkButton(master=tk, text="=", command=button_function, width=80, height=60, fg_color="#F875AA", hover_color="#FFC0D9")
+egal.place(x=265, y=410)
 
 tk.mainloop()
